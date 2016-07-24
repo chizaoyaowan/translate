2cf30d96d616ce26fdc3d64794eeb7e6d65fb2cb
chizaoyaowan <814230252@qq.com>
2016-07-24 18:47:31 +0800

    修复在不输入内容时继续翻译的bug
    +不输入内容时跳回，退出请直接点右上角

--- translate.py
+++ translate.py
@@ -4,13 +4,16 @@ import json
 import easygui as g
 import sys
 
+
 while True:
     url='http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule&smartresult=ugc&sessionFrom=https://www.baidu.com/link'
     msg="请输入需要翻译的文本："
     title='翻译小工具'
     default=''
     trans=g.enterbox(msg,title,default)
-
+    if trans=='':
+        continue
+    
     head={}
     head['User-Agent']='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36'
 
