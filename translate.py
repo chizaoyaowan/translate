ceaacc868f71cffdc12aa40c2e0a42d8db46bc88
chizaoyaowan <814230252@qq.com>
2016-07-27 18:12:56 +0800

    在有词典翻译结果的情况下，增加显示有道词典翻译结果

--- translate.py
+++ translate.py
@@ -4,16 +4,16 @@ import json
 import easygui as g
 import sys
 
-
 while True:
     url='http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule&smartresult=ugc&sessionFrom=https://www.baidu.com/link'
-    msg="请输入需要翻译的文本："
+
+    msg="请输入需要翻译的单词或短句："
     title='翻译小工具'
     default=''
     trans=g.enterbox(msg,title,default)
     if trans=='':
         continue
-    
+
     head={}
     head['User-Agent']='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36'
 
@@ -35,10 +35,22 @@ while True:
     html=response.read().decode('UTF-8')
 
     fanyi=json.loads(html)
-      if g.ccbox(fanyi['translateResult'][0][0]['tgt'],choices=('继续翻译','退出')):
+
+    late=fanyi['translateResult'][0][0]['tgt']
+    late=''.join(late)
+
+    if 'smartResult' in fanyi.keys():
+        
+        zhidian=fanyi['smartResult']["entries"]
+        zhidian=''.join(zhidian)
+        txt="\n有道翻译结果：\n"+late+"\n\n有道字典结果：\n"+zhidian
+    else :
+        txt="\n有道翻译结果：\n"+late
+
+    
+    if g.ccbox(txt,title='翻译结果',choices=('继续翻译','退出')):
         continue
     else:
         sys.exit(0)
-    
 
 
